2e7415057d2f1f268cbd7319c50a46fbc11e6034
Laurent Saives <etrenjeu@gmail.com>
2020-08-11 10:10:45 +0200

    version avec le bon destinataire de mail attendance

--- app/mailers/attendance_mailer.rb
+++ app/mailers/attendance_mailer.rb
@@ -1,16 +1,17 @@
 class AttendanceMailer < ApplicationMailer
 
-    default from: 'no-reply@event-brite-grenoble.fr'
+  default from: 'etrenjeu@gmail.com'
  
-  def attendance_email(attendance)
 
+  def attendance_email(attendance)
 
     @attendance = attendance
 
     @user = @attendance.user
 
     # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
-    mail(to: @user.email, subject: "Inscription à l'évènement " + @attendance.event.title) 
+    mail(to: @attendance.event.administrator.email, subject: "Inscription à l'évènement " + @attendance.event.title) 
   end
+
 
 end
